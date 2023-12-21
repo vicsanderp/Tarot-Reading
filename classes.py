1f4eeafc8141c409ccddcc189919971e0435700b
vicsanderp <victoruiz43@outlook.com>
2023-12-21 13:44:19 -0600

    Creating cards and deck

--- classes.py
+++ classes.py
@@ -6,7 +6,7 @@ class Card:
         self.meaning = meaning
     
     def __repr__(self):
-        return self.meaning
+        return '{0}: {1}'.format(self.name, self.meaning)
     
 class TarotDeck:
     def __init__(self, cards):
@@ -34,3 +34,29 @@ class Player:
     def showHand(self):
         for card in self.hand:
             print("{0}: {1}".format(card.name, card.meaning))
+
+cards = [
+    ['The Fool', 'Represents new beginnings, adventures, and a carefree approach.'],
+    ['The Magician', 'Symbolizes creative power, manifestation, and unlimited potential.'],
+    ['The High Priestess', 'Represents intuition, hidden mysteries, and inner knowledge.'],
+    ['The Empress', 'Symbolizes femininity, fertility, and creativity.'],
+    ['The Emperor', 'Represents authority, stability, and structure.'],
+    ['The Hierophant', 'Symbolizes spirituality, tradition, and connection to the divine.'],
+    ['The Lovers', 'Represents significant choices, relationships, and duality.'],
+    ['Justice', 'Symbolizes fairness, balance, and making just decisions.'],
+    ['The Hermit', 'Represents inner search, wisdom, and spiritual retreat.'],
+    ['Wheel of Fortune', 'Symbolizes the cycle of life, change, and evolution.'],
+    ['Strenght', 'Represents inner strength, courage, and controlling impulses.'],
+    ['The Hanged Man', 'Symbolizes surrender, letting go, and gaining a new perspective.'],
+    ['Death', 'Represents transformation, change, and rebirth.'],
+    ['Temperance', 'Symbolizes harmony, moderation, and seeking balance.'],
+    ['The Devil', 'Represents temptation, material bondage, and liberation.'],
+    ['The Tower', 'Symbolizes the destruction of outdated structures to allow change.'],
+    ['The Star', 'Represents hope, inspiration, and spiritual connection.'],
+    ['The Moon', 'Symbolizes intuition, confusion, and illusion.'],
+    ['The Sun', 'Represents clarity, joy, and vitality.'],
+    ['Judgement', 'Symbolizes evaluation, renewal, and accepting consequences.'],
+    ['The World', 'Represents fulfillment, integration, and complete success.']
+]
+
+mainDeck = TarotDeck(cards)
